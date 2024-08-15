5d2e20a8ef53cd631bce4d865291859cd36619a7
fumtas1k <fumta01a7@gmail.com>
2024-08-15 09:17:28 +0900

    [add]94追加

---
+++ hard/ruby/abc0147_d.rb
@@ -0,0 +1,16 @@
+# 94
+# ABC147/D
+# XOR
+
+M = 10 ** 9 + 7
+MAX_BIT = 60
+N = gets.to_i
+A = gets.split.map(&:to_i)
+
+ans = (MAX_BIT + 1).times.reduce(0) do |acc, i|
+  one = A.sum { _1[i] }
+  zero = N - one
+  (acc + zero * one * (1 << i) % M) % M
+end
+
+puts ans
